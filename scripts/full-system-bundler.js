d097c6b26e172ae9c602656a65654f6720e8e9ce
Marc Laval <mlaval@amadeus.com>
2016-10-12 01:13:54 +0200

    feat: full bundle with systemjs

---
+++ scripts/full-system-bundler.js
@@ -0,0 +1,37 @@
+const Builder = require("systemjs-builder");
+// SystemJS build options.
+var options = {
+  normalize: true,
+  runtime: false,
+  sourceMaps: true,
+  sourceMapContents: true,
+  minify: true,
+  mangle: true
+};
+var builder = new Builder('./dist/dev');
+builder.config({
+  defaultJSExtensions: true,
+  transpiler: false,
+  paths: {
+    "n:*": "./node_modules/*",
+    "rxjs/*": "node_modules/rxjs/*.js",
+  },
+  map: {
+    '@angular/common': 'n:@angular/common/index.js',
+    '@angular/compiler': 'n:@angular/compiler/index.js',
+    '@angular/core': 'n:@angular/core/index.js',
+    '@angular/forms': 'n:@angular/forms/index.js',
+    '@angular/http': 'n:@angular/http/index.js',
+    '@angular/platform-browser': 'n:@angular/platform-browser/index.js',
+    '@angular/platform-browser-dynamic': 'n:@angular/platform-browser-dynamic/index.js',
+    '@angular/platform-server': 'n:@angular/platform-server/index.js',
+    '@angular/router': 'n:@angular/router/index.js',
+    '@ng-bootstrap/ng-bootstrap': 'n:@ng-bootstrap/ng-bootstrap/index.js',
+    "rxjs": "n:rxjs"
+  },
+  packages: {
+    "app": {main: "main.jit.js", defaultExtension: "js"},
+  }
+});
+
+builder.bundle('app', 'dist/bundling-full/app.min.js', options);
