2239583e24e0371ee56d3ab791bd7c933ae79d42
Marc Laval <mlaval@amadeus.com>
2016-10-12 17:41:33 +0200

    feat: tree shaking with rollup

---
+++ rollup-jit.js
@@ -0,0 +1,23 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'app/main.jit.ts',
+  dest: 'dist/treeshaking-rollup/app.min.js',
+  sourceMap: false,
+  format: 'iife',
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+      "experimentalDecorators": true,
+      "emitDecoratorMetadata": true
+    }),
+    nodeResolve({jsnext: true, module: true}),
+    commonjs({
+      include: 'node_modules/rxjs/**',
+    }),
+    uglify()
+  ]
+}
