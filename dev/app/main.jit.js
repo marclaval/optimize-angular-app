015820942864d418fbadb5688e30e6e86a4d3bee
Marc Laval <mlaval@amadeus.com>
2016-11-14 16:26:38 +0100

    2016-11-14 at 16:26

--- dev/app/main.jit.js
+++ dev/app/main.jit.js
@@ -1,16 +1,20 @@
-System.register(['@angular/platform-browser-dynamic', './module'], function(exports_1, context_1) {
+System.register(['@angular/platform-browser-dynamic', '@angular/core', './module'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var platform_browser_dynamic_1, module_1;
+    var platform_browser_dynamic_1, core_1, module_1;
     return {
         setters:[
             function (platform_browser_dynamic_1_1) {
                 platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
             },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
             function (module_1_1) {
                 module_1 = module_1_1;
             }],
         execute: function() {
+            core_1.enableProdMode();
             platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(module_1.AppModule);
         }
     }
