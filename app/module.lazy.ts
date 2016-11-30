3d74c2ec001656006d2039b0367ed7f2e391f63e
Marc Laval <mlaval@amadeus.com>
2016-11-30 18:09:16 +0100

    feat: add Preloading to lazyloading

--- app/module.lazy.ts
+++ app/module.lazy.ts
@@ -1,6 +1,8 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Injectable} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes, PreloadingStrategy, Route} from '@angular/router';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 import {App} from './app';
 import {WelcomeModule} from './welcome/welcome.module';
@@ -13,9 +15,24 @@ const appRoutes: Routes = [
   {path: 'search', loadChildren: 'app/search/search.module#SearchModule'}
 ];
 
+@Injectable()
+export class PreloadSelectedModules implements PreloadingStrategy {
+  preloadedModules: string[] = [];
+
+  preload(route: Route, load: Function): Observable<any> {
+    if (route.path == 'list') {
+      this.preloadedModules.push(route.path);
+      return load();
+    } else {
+      return Observable.of(null);
+    }
+  }
+}
+
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes, {useHash: true}), WelcomeModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes, {useHash: true, preloadingStrategy: PreloadSelectedModules}), WelcomeModule],
   declarations: [App],
-  bootstrap: [App]
+  bootstrap: [App],
+  providers: [PreloadSelectedModules]
 })
 export class AppLazyModule {}
